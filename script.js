4d087301beb944e654a103ac31b6e52dc0c9f963
Aziz Berhouma <aziz.theprogrammer@gmail.com>
2023-09-14 10:17:58 +0200

    Developing Friends Section

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // Tasks List :
 let removeButtons = document.querySelectorAll(".wrapper .tasks .list .box i");
-console.log(removeButtons);
 
 removeButtons.forEach(function (button) {
 	button.onclick = function (e) {
 		button.parentElement.classList.toggle("completed");
+	}
+});
+
+// Friends Remove Button
+let friendsRemoveButtons = document.querySelectorAll(".wrapper .friend button");
+
+friendsRemoveButtons.forEach(function (button) {
+	button.onclick = function (e) {
+		e.target.parentElement.parentElement.parentElement.remove();
 	}
 });
